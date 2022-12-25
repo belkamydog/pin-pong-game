514ba2de8ef953b121b773f54b284550e5c2d188
belkamydog <belkamydog22@gmail.com>
2022-12-25 13:09:34 +0300

    add user interface

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include <time.h>
 
-const int LINE = 20; // размеры поля по y
+const int LINE = 21; // размеры поля по y
 const int COL = 50; // размеры поля по x
 const int SPEED_PL = 4; // скорость каретки
-const int BALL_SPEED = 20; //скорость мяча
+const int BALL_SPEED = 10; //скорость мяча
 
 void start_display(char screen[LINE][COL]);
-void show_display(char screen[LINE][COL]);
+void show_display(char screen[LINE][COL], int score, int time_m, int time_s);
 int move_roket(char screen[LINE][COL], int x,  char key);
 void ball(char *pflag, char *psides, int *pmx ,int *pmy);
+void game_time(int *ptime_m, int *ptime_s);
 
 int main()
 {
@@ -18,13 +20,21 @@ int main()
     int xr = 9;         // координата каретки
     int mx = 2, my = 4; // хранят координаты мяча
     int ball_box = 0;   // накопительная переменная скорости мяча
-    char key = ' ';
+    char key;
     char flag = 'l';
     char sides = 'r';
 
+    int score = 0;
+    int time_m = 0;
+    int time_s = 0;
+
+    printf("Hellow dear User!\nIt's a Pin-Pong-Game\nInput 's' for start new game or input q for out\nUse key_left or key_right to move platform\n");
+    printf("Input your choice and press enter: ");
+    scanf("%c", &key);
+
     while (key != 'q')
     {
-    timeout(5);
+    timeout(10);
     noecho();
     cbreak();
     stdscr = initscr();
@@ -34,7 +44,8 @@ int main()
     if (ball_box == BALL_SPEED) {ball(&flag, &sides, &mx, &my); ball_box = 0;}
     ball_box++;
     mass[my][mx] = '*';
-    show_display(mass);
+    game_time(&time_m, &time_s);
+    show_display(mass, score, time_m, time_s);
     doupdate();
     key = wgetch(stdscr);
     move_roket(mass, xr, key);
@@ -67,7 +78,7 @@ int move_roket(char screen[LINE][COL], int x, char key)
     return x;
 }
 
-void show_display(char screen[LINE][COL])
+void show_display(char screen[LINE][COL], int score, int time_m, int time_s)
 {
            for (int i = 0; i <= LINE-1; i++)
         {
@@ -75,6 +86,10 @@ void show_display(char screen[LINE][COL])
                 printw("%c", screen[i][j]);
             printw("\n");
         }
+    printw("\n");
+    printw("total score: %d     time: %d m %d s",score, time_m, time_s);
+    printw("\n");
+    printw("press q for out game");
 }
 
 void start_display(char screen[LINE][COL])
@@ -88,5 +103,13 @@ void start_display(char screen[LINE][COL])
                     else screen[i][j] = ' ';
                 }
         }
+
 }
 
+void game_time(int *ptime_m, int *ptime_s)
+{
+    int ms = clock();
+    int total_sek = ms / 100000;
+    (*ptime_m) = total_sek / 60;
+    (*ptime_s) = total_sek % 60;
+}
